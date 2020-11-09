bb20f994ae3782f98464c5edb551565e227546b0
Luan Rocha de Souza <luanrocha@outlook.com>
2020-11-09 00:27:28 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/luanrocha/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package br.com.luanrocha.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.luanrocha.workshopmongo.domain.Post;
+import br.com.luanrocha.workshopmongo.repository.PostRepository;
+import br.com.luanrocha.workshopmongo.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+	Optional<Post> obj = repo.findById(id);
+	return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
